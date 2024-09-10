a18e6164216f91d19de9c69ea8f0588a3aee9060
Lucas Rangel <elheus@gmail.com>
2024-09-10 13:08:26 -0300

    Header - Fase inicial

---
+++ frontend/src/app/teste/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { SideNavbar } from '@/components';
+import Body from '@/components/body';
+import Header from '@/components/header'
+import React from 'react'
+
+ 
+ const Teste = () => {
+  return (
+    <>
+    <Header headerTitle="Categorias" />
+    <SideNavbar />
+    </>
+  )
+}
+
+export default Teste;
+
