ae73e9c4a0e2f14e07b1d3ab752dc99023377da7
Marianne Gomes <mariannegomesm@gmail.com>
2024-10-03 16:35:50 -0300

    fix: inserting adjustments in redirect screens

--- frontend/src/components/card-collector/index.tsx
+++ frontend/src/components/card-collector/index.tsx
@@ -1,26 +1,29 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import styled, { css } from "styled-components";
-import { Categorias } from "@/lib/utils/constants";
+
+import { categorys } from "@/lib/utils/constants/category";
 
 const CardCollector = () => (
-
   <CardBodyDiv>
     <CardCollectorDiv>
-        {Categorias.map((categoria, indice ) => (
-            <Cards key={indice}>
-                <Image 
-                src={categoria.src} 
-                alt="imagem"
-                width={249} 
-                height={119}
-                style={{ borderRadius: '8px 8px 0px 0px', overflow: "hidden", filter: 'brightness(0.6)'}} 
-                />
-                <CardName>{categoria.nome}</CardName>
-            </Cards>
-        )
-        )}    
+      {categorys.map((categoria, indice) => (
+        <Cards key={indice}>
+          <Image
+            src={categoria.src}
+            alt="imagem"
+            width={249}
+            height={119}
+            style={{
+              borderRadius: "8px 8px 0px 0px",
+              overflow: "hidden",
+              filter: "brightness(0.6)",
+            }}
+          />
+          <CardName>{categoria.nome}</CardName>
+        </Cards>
+      ))}
     </CardCollectorDiv>
   </CardBodyDiv>
 );
@@ -30,14 +33,12 @@ const CardBodyDiv = styled.div(
     position: absolute;
     display: flex;
     z-index: 1;
-    
 
     top: 180px;
-    left:300px;
+    left: 300px;
 
     height: calc(100% - 180px);
     width: calc(100% - 300px);
- 
 
     color: ${theme.colors.black};
     background-color: ${theme.colors.blue[200]};
@@ -45,21 +46,21 @@ const CardBodyDiv = styled.div(
 );
 
 const CardCollectorDiv = styled.div(
- ({ theme }) => css`
+  ({ theme }) => css`
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    gap: 28px 28px; 
-    
+    gap: 28px 28px;
+
     max-width: calc(100% - 50px);
     max-height: 380px;
 
     margin-left: ${theme.space.s8};
     margin-top: ${theme.space.s7};
- `
+  `
 );
 
 const Cards = styled.div(
- ( { theme }) => css`
+  ({ theme }) => css`
     display: flex;
     flex-direction: column;
 
@@ -70,34 +71,32 @@ const Cards = styled.div(
 
     border-radius: 9.5px;
     border-width: 1px;
-    border-color: #C5C5C5;
+    border-color: #c5c5c5;
     border-style: solid;
     z-index: 2;
 
     justify-content: center;
     box-sizing: border-box;
 
-    &:hover, &:active {
-        cursor: pointer;
-        
-        background-color: ${theme.colors.black};
-        color: ${theme.colors.white};
+    &:hover,
+    &:active {
+      cursor: pointer;
 
-        transform: translateY(-3%);
-        -webkit-transition: transform 0.5s ease-out;
-        -o-transition: transform 0.5s ease-out;
-        transition: transform 0.5s ease-out;
+      background-color: ${theme.colors.black};
+      color: ${theme.colors.white};
 
-        box-shadow: 0px 4px 8px rgba(38, 38, 38, 0.2);
+      transform: translateY(-3%);
+      -webkit-transition: transform 0.5s ease-out;
+      -o-transition: transform 0.5s ease-out;
+      transition: transform 0.5s ease-out;
+
+      box-shadow: 0px 4px 8px rgba(38, 38, 38, 0.2);
     }
-
-    
- `
-)
+  `
+);
 
 const CardName = styled.p(
-    ( { theme }) => css`
-
+  ({ theme }) => css`
     display: flex;
     padding: ${theme.space.s4};
 
@@ -107,8 +106,7 @@ const CardName = styled.p(
 
     margin-left: 5px;
     letter-spacing: 0px;
-
-    `
-)
+  `
+);
 
 export default CardCollector;
