340aa028d01dd2a3bb3a88ee8872f6214bcd0184
Lucas Rangel <elheus@gmail.com>
2024-10-25 16:33:03 -0300

    "Adicionando Prettier para organizar melhor o código"

--- frontend/src/app/perfil/page.tsx
+++ frontend/src/app/perfil/page.tsx
@@ -1,22 +1,17 @@
-"use client"
+"use client";
 
-import HeaderPerfil from '@/components/header/perfil-index';
-import PerfilBody from '@/components/perfil';
-import SideNavbarPerfil from '@/components/side-navbar/index-perfil';
-
-
-
-
+import HeaderPerfil from "@/components/header/perfil-index";
+import PerfilBody from "@/components/perfil";
+import SideNavbarPerfil from "@/components/side-navbar/index-perfil";
 
 const Perfil = () => {
+  return (
+    <>
+      <HeaderPerfil />
+      <SideNavbarPerfil />
+      <PerfilBody />
+    </>
+  );
+};
 
-    return(
-        <>
-        <HeaderPerfil />
-        <SideNavbarPerfil />
-        <PerfilBody />
-        </>
-    )
-}
-
-export default Perfil
+export default Perfil;
--- frontend/src/app/senha/page.tsx
+++ frontend/src/app/senha/page.tsx
@@ -1,24 +1,17 @@
-"use client"
+"use client";
 
-import MudarSenhaForm from '@/components/forms/perfil-senha-form';
-import HeaderPerfil from '@/components/header/perfil-index';
-import PerfilBody from '@/components/perfil';
-import SideNavbarPerfil from '@/components/side-navbar/index-perfil';
-
-
-
-
-
+import MudarSenhaForm from "@/components/forms/perfil-senha-form";
+import HeaderPerfil from "@/components/header/perfil-index";
+import SideNavbarPerfil from "@/components/side-navbar/index-perfil";
 
 const PerfilSenha = () => {
+  return (
+    <>
+      <HeaderPerfil />
+      <SideNavbarPerfil />
+      <MudarSenhaForm />
+    </>
+  );
+};
 
-    return(
-        <>
-        <HeaderPerfil />
-        <SideNavbarPerfil />
-        <MudarSenhaForm />
-        </>
-    )
-}
-
-export default PerfilSenha
+export default PerfilSenha;
