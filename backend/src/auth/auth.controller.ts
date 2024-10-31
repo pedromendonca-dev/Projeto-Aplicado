94f200803aa64d35768721047f8ab5c1194a7c71
mariannegomesm <mariannegomesm@gmail.com>
2024-10-31 19:11:19 -0300

    feat: inserting validate google

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
+
+export interface UserProfile {
+  id: string;
+  email: string;
+  name: string;
+  photo?: string;
+}
+
+@Controller('auth')
+export class AuthController {
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth() {}
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(
+    @Req() req: Request & { user: UserProfile },
+    @Res() res: Response,
+  ) {
+    res.redirect('http://localhost:3002/categorias');
+  }
+}
